e11605719aa361d7d5e878618a043a05a0458d92
Josh Fogg <jfogg@duck.com>
2023-05-01 13:17:39 +0000

    Sync LeetCode submission - Two Sum (python3)

---
+++ problems/two_sum/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        twoSum takes a list of integers (nums) and an integer
+        (target) as inputs and then returns then returns [i,j]
+        where nums[i] + nums[j] == target. It is assumed that
+        such a pair of indexes exist and that nums is at least
+        two items long.
+        """
+
+        # create a location map, O(n)
+        locations = {nums[i]: i for i in range(len(nums))}
+
+        # iteracte over values, O(n)
+        for i in range(len(nums)):
+            try:
+                # compute nums[j] = target - nums[i] and then
+                # try to access j from the location map 
+                j = locations[target - nums[i]]
+                # can't reuse values
+                if j == i: continue
+                # if error wasn't thrown, done!
+                return [i, j]
+            # if nums[j] not in nums, KeyError will be thrown
+            except KeyError:
+                continue
