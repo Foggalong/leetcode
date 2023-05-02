7f260531d4bc3a0d18e2251505ee711a01b7117b
Josh Fogg <jfogg@duck.com>
2023-05-02 11:26:43 +0000

    Sync LeetCode submission - Ransom Note (python3)

---
+++ problems/ransom_note/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        """
+        Takes two strings (ransomNote, magazine) as input and then
+        returns True if the first can be constructed from the letters
+        of the second without replacement, or False otherwise.
+        """
+
+        # create a letter count dictionary
+        magazine_dict = {chr(i): 0 for i in range(97,123)}
+        for letter in magazine:
+            magazine_dict[letter] +=1
+        
+        # work through ransomNote taking letters from magazine
+        for letter in ransomNote:
+            if magazine_dict[letter] > 0:
+                magazine_dict[letter] -= 1
+            else:
+                return False
+
+        return True
