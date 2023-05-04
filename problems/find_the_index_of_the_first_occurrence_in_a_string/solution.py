0287636ec9d851a64edf3517e25384fb6eb66748
Josh Fogg <jfogg@duck.com>
2023-05-04 11:43:35 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String (python3)

---
+++ problems/find_the_index_of_the_first_occurrence_in_a_string/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    # Give two strings, 'haystack' and 'needle', each of
+    # these functions returns the index of needles first
+    # occurrence in haystack, or -1 if it never appears.
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        """
+        Use Python's in-build `string.find(chr)`
+        """
+        return haystack.find(needle)
+
+    def strStr1(self, haystack: str, needle: str) -> int:
+        """
+        Compare every substring of length needle.
+        """
+        n = len(needle)  # avoid taking it twice
+        for i in range(len(haystack)-n+1):
+            # compare each substring of len(needle)
+            if haystack[i:i+n] == needle: return i
+        return -1
+    
+    def strStr(self, haystack: str, needle: str) -> int:
+        """
+        Remove first character of haystack until it starts
+        with needle.
+        """
+        i = 0  # count how many characters removed
+        while haystack:
+            if haystack.startswith(needle):
+                return i
+            else:
+                # remove first character of haystack
+                haystack = haystack[1:]
+                i += 1
+        return -1
+
