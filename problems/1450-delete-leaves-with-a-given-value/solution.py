3b765aa018e53776296dce961ef2f41d6be473bd
Josh Fogg <jfogg@duck.com>
2024-05-17 12:15:22 +0000

    Sync LeetCode submission Runtime - 48 ms (40.20%), Memory - 17.1 MB (80.39%)

--- problems/1450-delete-leaves-with-a-given-value/solution.py
+++ problems/1450-delete-leaves-with-a-given-value/solution.py
@@ -10,33 +10,19 @@ class Solution:
         """
         Given a binary tree `root` and an integer `target`, delete all the leaf nodes with
         value `target`. It does this by recursively examining the left and right branches,
-        pruning any target leaves with pruneBranch as necessary.
+        pruning any target leaves as necessary.
         """
 
-        # NOTE in testing I found hardcoding the base case `root is None` actually slowed
-        # down the code (rather than sped up as we might expect) presumably due to the
-        # check being needed every time removeLeafNodes was called recursively.
+        # cases where branch empty or is just the target leaf
+        if (root is None) or self.isTargetLeaf(root, target): return None
 
-        root.left = self.pruneBranch(root.left, target)
-        root.right = self.pruneBranch(root.right, target)
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
 
         # if the root has become a target leaf, prune and finish
         if self.isTargetLeaf(root, target): return None
 
         return root
-
-
-    def pruneBranch(self, branch: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        """
-        Helper function which does the actual pruning of the leaves from a given branch.
-        If the branch is already empty or just consists of the target leaf it returns None,
-        otherwise it recursively prunes further down the branch using removeLeafNodes.
-        """
-
-        # cases where branch empty or is just the target leaf
-        if (branch is None) or self.isTargetLeaf(branch, target): return None
-        # cases where there's more leaves down branch
-        return self.removeLeafNodes(branch, target)
 
 
     def isTargetLeaf(self, root: Optional[TreeNode], target: int) -> bool:
