d6f137769fb8668b0f8ce6dd2ee95696116d390d
Josh Fogg <jfogg@duck.com>
2024-05-21 11:00:06 +0000

    Sync LeetCode submission Runtime - 3430 ms (6.33%), Memory - 16.8 MB (30.01%)

---
+++ problems/0078-subsets/solution.py
@@ -0,0 +1,43 @@
+from itertools import chain, combinations
+
+"""
+It was odd that this problem came up today, because I used the powerset
+recipe from the itertools module to do yesterday's Daily LeetCode. This
+is included as `powerset` below. Today's solution was also started at a
+time long past, so I finished that rather than starting anew. It is for
+sure not the best way to do this. 
+"""
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """List all subserts using recurrsive function"""
+
+        n: int = len(nums)
+
+        # start with the base case, the emptyset and itself
+        powerset: List[List[int]] = [[], nums]
+        if n == 1: return powerset
+
+        # top level iteration over the elements in nums
+        for i in range(n):
+            # add set with the ith element removed
+            iless_nums: List[int] = nums[:i] + nums[i+1:]
+            powerset.append(iless_nums)
+            # now consider the subsets of this set
+            for iless_subset in self.subsets(iless_nums):
+                # add any newly found subsets
+                if iless_subset not in powerset:
+                    powerset.append(iless_subset)
+
+        return powerset
+
+
+    def powerset(self, nums: Iterable[int]):
+        """
+        Return the power set of an iterable, excluding the empty set. Example:
+        powerset([1,2,3]) → (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+        """
+        # starting the range from 1, not 0, skips the emptyset
+        return chain.from_iterable(combinations(nums, r) for r in range(1, len(nums)+1))
+
